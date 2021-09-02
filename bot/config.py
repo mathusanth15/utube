1c3807b164954413921da57ad488f7027e92f808
mathusanth15 <81505953+mathusanth15@users.noreply.github.com>
2021-09-02 10:06:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,26 +3,26 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("1991699635:AAEKPca5JvN4pxusGNTf-m7C4kxjK_GcIvU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-    SESSION_NAME = os.environ.get("YouTubeUploader", ":memory:")
+    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
 
-    API_ID = int(os.environ.get("5615631"))
+    API_ID = int(os.environ.get("API_ID"))
 
-    API_HASH = os.environ.get("f65d600ff580456871cfddc086337b7a")
+    API_HASH = os.environ.get("API_HASH")
 
-    CLIENT_ID = os.environ.get("921912615039-42h2eg2tsqcmbig6mae5df8vai54mbff.apps.googleusercontent.com")
+    CLIENT_ID = os.environ.get("CLIENT_ID")
 
-    CLIENT_SECRET = os.environ.get("GnsnVpyT3DT9cJStuexDOJ55")
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-    BOT_OWNER = int(os.environ.get("1496802577"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 
-    AUTH_USERS_TEXT = os.environ.get("1496802577", "")
+    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
     AUTH_USERS = [BOT_OWNER, 374321319] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
-        felse []
+        else []
     )
 
     VIDEO_DESCRIPTION = (
@@ -41,9 +41,58 @@ class Config:
 
     UPLOAD_MODE = os.environ.get("UPLOAD_MODE") or False
     if UPLOAD_MODE:
-        if UPLOAD_MODE.lower() in ["public"]:
+        if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
             UPLOAD_MODE = UPLOAD_MODE.lower()
         else:
-            UPLOAD_MODE = true
+            UPLOAD_MODE = False
+
+    CRED_FILE = "auth_token.txt"
+import os
+
+
+class Config:
+
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+
+    API_ID = int(os.environ.get("API_ID"))
+
+    API_HASH = os.environ.get("API_HASH")
+
+    CLIENT_ID = os.environ.get("CLIENT_ID")
+
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+
+    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+
+    AUTH_USERS = [BOT_OWNER, 374321319] + (
+        [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
+        if AUTH_USERS_TEXT
+        else []
+    )
+
+    VIDEO_DESCRIPTION = (
+        os.environ.get("VIDEO_DESCRIPTION", "").replace("<", "").replace(">", "")
+    )
+
+    VIDEO_CATEGORY = (
+        int(os.environ.get("VIDEO_CATEGORY")) if os.environ.get("VIDEO_CATEGORY") else 0
+    )
+
+    VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "")
+
+    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "")
+
+    DEBUG = bool(os.environ.get("DEBUG"))
+
+    UPLOAD_MODE = os.environ.get("UPLOAD_MODE") or False
+    if UPLOAD_MODE:
+        if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
+            UPLOAD_MODE = UPLOAD_MODE.lower()
+        else:
+            UPLOAD_MODE = False
 
     CRED_FILE = "auth_token.txt"
